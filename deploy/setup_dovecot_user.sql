4f83b19ef05a9347f7f4d3b814c320117f0e4f9f
Andrew Mason <andrew@nocturnal.net.au>
2016-07-16 00:55:40 +0930

    Ensure that we have dedicated dovecot user

--- deploy/setup_dovecot_user.sql
+++ deploy/setup_dovecot_user.sql
@@ -5,6 +5,8 @@ BEGIN;
 use mailtest;
 
 GRANT select on mailtest.* to 'dovecot'@'%' identified by 'test';
+GRANT select,delete,insert,update on mailtest.quota to 'dovecot'@'%';
+GRANT select,update,delete,insert on mailtest.expires to 'dovecot'@'%';
 
 
 COMMIT;
